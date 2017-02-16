d075c526876ab1b68223b4ee8957440bc5d70f68
Merlin de la Haye <Merlin de la Haye>
2017-02-16 19:13:15 +0100

    fixes OrganizeImports error with java.lang.Math

--- src/mbeb/opengldefault/rendering/renderable/BezierCurveRenderable.java
+++ src/mbeb/opengldefault/rendering/renderable/BezierCurveRenderable.java
@@ -20,16 +20,16 @@ import org.lwjgl.opengl.*;
 public class BezierCurveRenderable implements IRenderable {
 
 	private static final String TAG = "BezierCurveRenderable";
-	private BezierCurve curve;
-	private VAORenderable renderable;
+	private final BezierCurve curve;
+	private final VAORenderable renderable;
 
-	public BezierCurveRenderable(BezierCurve curve) {
+	public BezierCurveRenderable(final BezierCurve curve) {
 		this.curve = curve;
-		float[] vertexData = {0, 1};
+		final float[] vertexData = {0, 1};
 
-		int[] indexData = {0, 1};
+		final int[] indexData = {0, 1};
 
-		BoundingBox bb = generateBoundingBox();
+		final BoundingBox bb = generateBoundingBox();
 
 		renderable = new VAORenderable(vertexData, indexData, new DataFragment[] {DataFragment.FLOAT}, bb);
 	}
@@ -40,36 +40,36 @@ public class BezierCurveRenderable implements IRenderable {
 	 * @return the calculated 3D AABB
 	 */
 	private BoundingBox generateBoundingBox() {
-		Vector3f curveStart = curve.getControlPoints().get(0);
+		final Vector3f curveStart = curve.getControlPoints().get(0);
 		float minX = curveStart.x, minY = curveStart.y, minZ = curveStart.z, maxX = curveStart.x, maxY = curveStart.y, maxZ = curveStart.z;
 
-		for (Vector3f curvePoint : curve.getControlPoints()) {
-			minX = Math.min(minX, curvePoint.x);
-			minY = Math.min(minY, curvePoint.y);
-			minZ = Math.min(minZ, curvePoint.z);
+		for (final Vector3f curvePoint : curve.getControlPoints()) {
+			minX = java.lang.Math.min(minX, curvePoint.x);
+			minY = java.lang.Math.min(minY, curvePoint.y);
+			minZ = java.lang.Math.min(minZ, curvePoint.z);
 
-			maxX = Math.max(maxX, curvePoint.x);
-			maxY = Math.max(maxY, curvePoint.y);
-			maxZ = Math.max(maxZ, curvePoint.z);
+			maxX = java.lang.Math.max(maxX, curvePoint.x);
+			maxY = java.lang.Math.max(maxY, curvePoint.y);
+			maxZ = java.lang.Math.max(maxZ, curvePoint.z);
 		}
 		return new BoundingBox(new Vector3f(minX, minY, minZ), new Vector3f(maxX - minX, maxY - minY, maxZ - minZ));
 	}
 
 	@Override
-	public void render(Shader shader) {
+	public void render(final Shader shader) {
 		shader.use();
 		GL11.glLineWidth(3);
 
 		int modelUniform = shader.getUniform("bernstein");
 
-		FloatBuffer bernsteinBuffer = BufferUtils.createFloatBuffer(16);
+		final FloatBuffer bernsteinBuffer = BufferUtils.createFloatBuffer(16);
 		GL20.glUniformMatrix4fv(modelUniform, false, curve.bernstein().get(bernsteinBuffer));
 		GLErrors.checkForError(TAG, "glUniformMatrix4fv");
 
-		for (Matrix4f bezierMatrix : curve.getBezierMatrices()) {
+		for (final Matrix4f bezierMatrix : curve.getBezierMatrices()) {
 			modelUniform = shader.getUniform("bezier");
 
-			FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
+			final FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
 			GL20.glUniformMatrix4fv(modelUniform, false, bezierMatrix.get(buffer));
 			GLErrors.checkForError(TAG, "glUniformMatrix4fv");
 
