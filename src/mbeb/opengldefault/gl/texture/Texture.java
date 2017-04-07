f85d1c2a31a19630b6e7c5c8ef4511c6f857447b
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-04-08 00:24:07 +0200

    Added additional texture enumerations

--- src/mbeb/opengldefault/gl/texture/Texture.java
+++ src/mbeb/opengldefault/gl/texture/Texture.java
@@ -1,8 +1,11 @@
 package mbeb.opengldefault.gl.texture;
 
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
 import static org.lwjgl.opengl.GL12.GL_TEXTURE_3D;
+import static org.lwjgl.opengl.GL13.GL_CLAMP_TO_BORDER;
 import static org.lwjgl.opengl.GL13.GL_TEXTURE_CUBE_MAP;
+import static org.lwjgl.opengl.GL14.GL_MIRRORED_REPEAT;
 import static org.lwjgl.opengl.GL30.GL_TEXTURE_2D_ARRAY;
 
 import mbeb.opengldefault.gl.GLObject;
@@ -15,6 +18,11 @@ import mbeb.opengldefault.gl.GLObject;
  */
 public abstract class Texture extends GLObject {
 
+	//<editor-fold desc="Enumerations">
+	/**
+	 * The type of a OpenGL texture.
+	 * The constants of this enumerated type provide the value of the OpenGL enumeration to match their type.
+	 */
 	public enum Type {
 		/**
 		 * A two dimensional texture.
@@ -39,9 +47,128 @@ public abstract class Texture extends GLObject {
 			this.glEnum = glEnum;
 		}
 
-		public int getGlEnum() {
+		/**
+		 * @return the OpenGL enum representing this texture type
+		 */
+		public int getGLEnum() {
+			return glEnum;
+
+		}
+	}
+
+	/**
+	 * The wrap mode of a OpenGL texture.
+	 * This determines how sampling out of texture range is handled.
+	 * The constants of this enumerated type provide the value of the OpenGL enumeration to match their mode.
+	 */
+	public enum WrapMode {
+		/**
+		 * If the texture is sampled outside of its range, a user specified border color is returned.
+		 */
+		CLAMP_TO_BORDER(GL_CLAMP_TO_BORDER),
+		/**
+		 * If the texture is sampled outside of its range, the texture coordinate is clamped to its range.
+		 */
+		CLAMP_TO_EDGE(GL_CLAMP_TO_EDGE),
+		/**
+		 * If the texture is sampled outside of its range, the texture is sampled as if mirrored.
+		 * Please note this is comparable to placing oneself between to mirrors, so if you sample further out of range,
+		 * the results are the original texture once again.
+		 */
+		MIRRORED_REPEAT(GL_MIRRORED_REPEAT),
+		/**
+		 * If the texture is sampled outside of its range, the texture repeats itself.
+		 * This is comparable to taking the texture coordinate modulo 1.0.
+		 */
+		REPEAT(GL_REPEAT);
+
+		private int glEnum;
+
+		WrapMode(int glEnum) {
+			this.glEnum = glEnum;
+		}
+
+		/**
+		 * @return the OpenGL enum representing this wrap mode
+		 */
+		public int getGLEnum() {
 			return glEnum;
 		}
 	}
+
+	/**
+	 * The minification filter of an OpenGL texture.
+	 * This determines how samples of an area larger than the texel size are handled.
+	 */
+	public enum MinificationFilter {
+		/**
+		 * The value of the nearest (Manhattan-Distance) texel is returned.
+		 */
+		NEAREST(GL_NEAREST),
+		/**
+		 * The four nearest texels are weighted based on distance.
+		 */
+		LINEAR(GL_LINEAR),
+		/**
+		 * The value is calculated by using NEAREST on the mipmap level closest to the desired level of detail.
+		 */
+		NEAREST_MIPMAP_NEAREST(GL_NEAREST_MIPMAP_NEAREST),
+		/**
+		 * The value is calculated by using LINEAR on the mipmap level closest to the desired level of detail.
+		 */
+		LINEAR_MIPMAP_NEAREST(GL_LINEAR_MIPMAP_NEAREST),
+		/**
+		 * The values of NEAREST on the two closest mipmap levels are weighted based on distance.
+		 */
+		NEAREST_MIPMAP_LINEAR(GL_NEAREST_MIPMAP_LINEAR),
+		/**
+		 * The values of LINEAR on the two closest mipmap levels are weighted based on distance.
+		 */
+		LINEAR_MIPMAP_LINEAR(GL_LINEAR_MIPMAP_LINEAR);
+
+		private int glEnum;
+
+		MinificationFilter(int glEnum) {
+			this.glEnum = glEnum;
+		}
+
+		/**
+		 * @return the OpenGL enum representing this minification filter
+		 */
+		public int getGLEnum() {
+			return glEnum;
+		}
+	}
+
+	/**
+	 * The magnification filter of an OpenGL texture.
+	 * This determines how samples of an area smaller than the texel size are handled.
+	 */
+	public enum MagnificationFilter {
+		/**
+		 * The value of the nearest (Manhattan-Distance) texel is returned.
+		 */
+		NEAREST(GL_NEAREST),
+		/**
+		 * The four nearest texels are weighted based on distance.
+		 */
+		LINEAR(GL_LINEAR);
+
+		private int glEnum;
+
+		MagnificationFilter(int glEnum) {
+			this.glEnum = glEnum;
+		}
+
+		/**
+		 * @return the OpenGL enum representing this magnification filter
+		 */
+		public int getGLEnum() {
+			return glEnum;
+		}
+	}
+	//</editor-fold>
+
+
 
 }
