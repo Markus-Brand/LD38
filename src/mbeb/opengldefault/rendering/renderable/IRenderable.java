cd1fc8ceb43f0e7d0eb8e320f39fd8a6d3f697eb
Erik Brendel <erik.brendel@student.hpi.de>
2017-04-21 16:26:01 +0200

    a bit feedback

--- src/mbeb/opengldefault/rendering/renderable/IRenderable.java
+++ src/mbeb/opengldefault/rendering/renderable/IRenderable.java
@@ -43,7 +43,7 @@ public interface IRenderable extends IRenderableHolder{
 	
 	/**
 	 * create an IRenderable that uses a Material for rendering
-	 * @param material the material that `this` should use for rendering
+	 * @param material the material that <i><code>this</code></i> should use for rendering
 	 * @return an IRenderable that renders itself with the given material
 	 */
 	default IRenderable withMaterial(Material material) {
