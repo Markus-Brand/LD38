b8efc733193829b83269848acc62ff3dfe16de21
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-04-22 15:12:59 +0200

    Stuff and things and stuff and things 2

---
+++ src/mbeb/opengldefault/dungeon/DungeonLevel.java
@@ -0,0 +1,7 @@
+package mbeb.opengldefault.dungeon;
+
+/**
+ * Level up
+ */
+public class DungeonLevel {
+}
---
+++ src/mbeb/opengldefault/dungeon/room/Slot.java
@@ -0,0 +1,11 @@
+package mbeb.opengldefault.dungeon.room;
+
+import mbeb.opengldefault.animation.BoneTransformation;
+
+/**
+ * Insert here
+ */
+public class Slot {
+    BoneTransformation transformation;
+    
+}
