795cfad4bcb1776fd68402b6686a2db51c9bfeb3
Markus-PC\Markus <markus.brand@student.hpi.de>
2017-04-22 10:54:00 +0200

    Add basic intro and fix res directory issue

--- src/mbeb/opengldefault/game/IntroGameState.java
+++ src/mbeb/opengldefault/game/IntroGameState.java
@@ -1,29 +1,50 @@
 package mbeb.opengldefault.game;
 
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glViewport;
+
+import org.lwjgl.glfw.GLFW;
+
+import mbeb.opengldefault.controls.KeyBoard;
+import mbeb.opengldefault.gl.GLContext;
+import mbeb.opengldefault.logging.GLErrors;
+
 public class IntroGameState implements GameState {
+
+	private static final String TAG = "IntroGameState";
+
+	private float progress;
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-
+		progress = 0;
 	}
 
 	@Override
 	public void update(double deltaTime) {
-		// TODO Auto-generated method stub
-
+		if (KeyBoard.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) {
+			progress = 1;
+		}
+		progress += deltaTime;
 	}
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		glClearColor(progress, progress, progress, 1);
+		GLErrors.checkForError(TAG, "glClearColor");
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		GLErrors.checkForError(TAG, "glClear");
 
+		glViewport(0, 0, GLContext.getFramebufferWidth(), GLContext.getFramebufferHeight());
+		GLErrors.checkForError(TAG, "glViewport");
 	}
 
 	@Override
 	public GameStateIdentifier getNextState() {
-		// TODO Auto-generated method stub
-		return null;
+		return progress >= 1 ? GameStateIdentifier.OVERWORLD : null;
 	}
 
 	@Override
@@ -34,8 +55,7 @@ public class IntroGameState implements GameState {
 
 	@Override
 	public void open() {
-		// TODO Auto-generated method stub
-
+		progress = 0;
 	}
 
 }
