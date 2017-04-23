dfc1b64c7552cdb98a73b3ab66ef8e95ce08fe3d
Markus-PC\Markus <markus.brand@student.hpi.de>
2017-04-23 19:37:56 +0200

    Player Entity and Behaviour

--- src/mbeb/lifeforms/PlayerEntity.java
+++ src/mbeb/lifeforms/PlayerEntity.java
@@ -1,11 +1,29 @@
 package mbeb.lifeforms;
 
+import mbeb.opengldefault.animation.AnimationStateFacade;
 import mbeb.opengldefault.scene.*;
+import mbeb.opengldefault.scene.behaviour.CombinedBehaviour;
+import mbeb.opengldefault.scene.behaviour.HeightSource;
+import mbeb.opengldefault.scene.behaviour.SamuraiPlayerBehaviour;
+import mbeb.opengldefault.scene.behaviour.WalkOnHeightMapBehaviour;
 
 public class PlayerEntity extends LifeformEntity {
 
-	public PlayerEntity(final SceneObject sceneObject, final float healthpoints) {
+	private AnimationStateFacade animator;
+
+	private float playerSpeed = 4f;
+
+	public PlayerEntity(final SceneObject sceneObject, AnimationStateFacade animator,
+			final float healthpoints, HeightSource heightSource) {
 		super(sceneObject, healthpoints);
+		this.animator = animator;
+		addBehaviour(0, new CombinedBehaviour(
+				new SamuraiPlayerBehaviour(),
+				new WalkOnHeightMapBehaviour(heightSource, playerSpeed)));
+	}
+
+	public AnimationStateFacade getAnimator() {
+		return animator;
 	}
 
 }
