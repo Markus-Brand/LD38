2b92f39dab745347c90672e73e414c08c6e6212a
Erik Brendel <erik.brendel@student.hpi.de>
2017-04-24 16:47:34 +0200

    goblin texture

--- src/mbeb/lifeforms/Goblin.java
+++ src/mbeb/lifeforms/Goblin.java
@@ -28,7 +28,7 @@ public class Goblin extends Monster {
 		mesh = new ObjectLoader().loadFromFileAnim("goblin.fbx");
 		mesh.setTransform(new Matrix4f().rotate(new AxisAngle4f((float) (Math.PI) / -2, 1, 0, 0)));
 		mesh.getSkeleton().printRecursive("");
-		material = new ColorMaterial(Color.ORANGE);
+		material = new Material("material/goblin", 2);
 
 		this.animationShader = animationShader;
 
