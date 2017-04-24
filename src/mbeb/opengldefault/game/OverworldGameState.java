3fd0348b0b507cb432dce8527b56db3e0c9ceac5
Erik Brendel <erik.brendel@student.hpi.de>
2017-04-25 00:47:21 +0200

    al error fix

--- src/mbeb/opengldefault/game/OverworldGameState.java
+++ src/mbeb/opengldefault/game/OverworldGameState.java
@@ -244,13 +244,13 @@ public class OverworldGameState implements GameState {
 		shared.playerEntity.addTo(scene.getSceneGraph(), scene.getLightManager());
 		shared.playerEntity.setHeightSource(playerHeight);
 		shared.playerEntity.setPosition(port);
-		shared.playerEntity.setStoneWalkingSound(false);
 		if (leftForDungeon) {
 			leftForDungeon = false;
 		}
 		scene.getLightManager().rewriteUBO();
 		GLContext.hideCursor();
 		scene.getSoundEnvironment().makeCurrent();
+		shared.playerEntity.setStoneWalkingSound(false);
 	}
 
 }
